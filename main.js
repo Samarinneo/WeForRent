924e81e7783a46634e1c8affe5b2de1bddf829fb
Samarinneo <54540423+Samarinneo@users.noreply.github.com>
2020-04-18 15:54:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    $('.slider').slick({
+    	arrows:false,
+    	infinite: true,
+    	  autoplay: true,
+  autoplaySpeed: 1500,
+    fade: true,
+  cssEase: 'linear'
+    });
+});
